650a2a995692b3e5515d980389580f52a7c84abe
dougiemath <douglasimatheson@gmail.com>
2021-08-31 13:21:43 +0000

    function to display username in title added

--- assets/js/gamescript.js
+++ assets/js/gamescript.js
@@ -15,6 +15,14 @@ function startGame(){
     console.log("start game function")
 }
 
+//function to display start message
+
+function displayStartMessage() {
+    let username = document.getElementById("username-input").value;
+    document.getElementById("instructions-message").textContent = "Hi " + username + ", before you start the exercise, please read the langauge point below."
+    document.getElementById("ready-message").textContent = "If you are ready, " + username + ", you can click the button to start the exercise!"
+}
+
 // function to call questions
 
 // function to check answers
