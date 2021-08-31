958ce34bb401c8f202e7ce329c7a4b1cc374988e
dougiemath <douglasimatheson@gmail.com>
2021-08-31 12:52:03 +0000

    add functionality to username form / log username

--- assets/js/gamenavigation.js
+++ assets/js/gamenavigation.js
@@ -1,1 +1,6 @@
-// functions to hide + display divs
+
+// function to display welcome/start message
+
+function displayStartMessage(){
+    console.log("test button working")
+}
--- assets/js/gamescript.js
+++ assets/js/gamescript.js
@@ -2,12 +2,23 @@
 
 // question list (array)
 
-// function to display welcome/start message
+// function to get username
+
+function addUsername(){
+    let usernameInput = document.getElementById("username-input").value
+    console.log(usernameInput);
+}
 
 // function to startGame
+
+function startGame(){
+    console.log("start game function")
+}
 
 // function to call questions
 
 // function to check answers
 
 // function to display feedback
+
+
