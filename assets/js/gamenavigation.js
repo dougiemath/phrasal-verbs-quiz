1b27e47383f8af1ba7b8a052d5fb1c2f544a8c09
dougiemath <douglasimatheson@gmail.com>
2021-09-01 10:40:06 +0000

    add functions to hide user input and langauge point divs

--- assets/js/gamenavigation.js
+++ assets/js/gamenavigation.js
@@ -4,3 +4,27 @@
  *  */
 
  document.getElementById("username-input").focus()
+
+/**
+ * Hides the username-input div
+ *  */
+
+function hideUsername(){
+    let usernameContainer = document.getElementById("username-container")
+
+    if (usernameContainer.style.display = "block")
+        usernameContainer.style.display = "none"
+}
+
+/**
+ * Hides the language point div
+ *  */
+
+ function hideLanguageContainer(){
+    let languageContainer = document.getElementById("language-point-container")
+
+    if (languageContainer.style.display = "block")
+    languageContainer.style.display = "none"
+}
+
+
