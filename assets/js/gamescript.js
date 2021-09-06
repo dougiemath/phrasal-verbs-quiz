f5abf8ebb8de626f6fba3828e94cb655f7ec2932
dougiemath <douglasimatheson@gmail.com>
2021-09-06 16:06:14 +0000

    Fixed spelling error

--- assets/js/gamescript.js
+++ assets/js/gamescript.js
@@ -79,7 +79,7 @@ function showHide(target) {
 
 function displayStartMessage() {
     let username = document.getElementById("username-input").value;
-    document.getElementById("instructions-message").textContent = "Hi " + username + ", before you start the exercise, please read the langauge point below.";
+    document.getElementById("instructions-message").textContent = "Hi " + username + ", before you start the exercise, please read the language point below.";
     document.getElementById("ready-message").textContent = "If you are ready, " + username + ", you can click the button to start the exercise!";
 }
 
